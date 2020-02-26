322fd38120b58b441359ff8ceb236a27d10570db
Gerald2k <55755923+Gerald2k@users.noreply.github.com>
2020-02-26 15:40:56 +0000

    Update SettingsMenu.cs
    oH crAp

--- PrototypeV2/Assets/Scripts/SettingsMenu.cs
+++ PrototypeV2/Assets/Scripts/SettingsMenu.cs
@@ -4,15 +4,9 @@ using UnityEngine;
 
 public class SettingsMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+   public void SetVolume (float volumne)
     {
-        
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }

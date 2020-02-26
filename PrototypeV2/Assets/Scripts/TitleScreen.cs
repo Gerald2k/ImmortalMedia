b3671ee5dfe00ccec1bb874111f35a92577d3f12
Gerald2k <55755923+Gerald2k@users.noreply.github.com>
2020-02-26 15:38:14 +0000

    added settings menu
    hello i changed things luv

---
+++ PrototypeV2/Assets/Scripts/TitleScreen.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+using UnityEngine.SceneManagement;
+
+public class TitleScreen : MonoBehaviour
+{
+    public Button yourButton;
+    public void Start()
+    {
+        Button btn = yourButton.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+
+    }
+
+    public void TaskOnClick()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); 
+    }
+}
+
+  
